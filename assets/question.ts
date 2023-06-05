444e28b9c97334282267bb542dba7f24c75f4880
kandalog <asuasu.tee@gmail.com>
2023-06-05 17:24:53 +0900

    ピンインモード用にenを追加

--- assets/question.ts
+++ assets/question.ts
@@ -1,13 +1,14 @@
-import { pinyin, ch, jp } from "./data";
+import { pinyin, ch, jp, en } from "./data";
 
 type questionType = {
-  jp: string;
-  ch: string;
-  pi: string;
+  jp?: string;
+  ch?: string;
+  pi?: string;
+  en?: string;
 };
 
 export const questions: questionType[] = [];
 
 for (let i = 0; i < pinyin.length; i++) {
-  questions.push({ jp: jp[i], ch: ch[i], pi: pinyin[i] });
+  questions.push({ jp: jp[i], ch: ch[i], pi: pinyin[i], en: en[i] });
 }
