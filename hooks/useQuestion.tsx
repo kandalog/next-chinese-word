f90e98f467bdeddb9a37590b8769e84ae84a4de2
kandalog <asuasu.tee@gmail.com>
2023-06-05 17:25:27 +0900

    useState化 + ピンインモード追加

---
+++ hooks/useQuestion.tsx
@@ -0,0 +1,81 @@
+import { questions as materialQuestion } from "@/assets/question";
+import { useState } from "react";
+
+export const useQuestion = () => {
+  const [questions, setQuestions] = useState(materialQuestion);
+
+  // データを押した時の処理
+  const handleOnClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    const target = e.currentTarget.nextElementSibling;
+    target && target.classList.toggle("active");
+  };
+
+  // 消す時の処理
+  const removeActiveClass = (e: React.MouseEvent<HTMLDivElement>) => {
+    e.currentTarget.classList.remove("active");
+  };
+
+  // シャッフルボタン
+  const shuffleButton = () => {
+    setQuestions((prevState) => shuffle([...prevState]));
+  };
+
+  // 漢字を問題として表示する
+  const reverseQuestion = () => {
+    // ピンインモードの場合は起動させない
+    if (!questions[0].en) return;
+    setQuestions((prevState) => {
+      const newState = prevState.map((data) => {
+        return {
+          jp: data.jp,
+          ch: data.pi,
+          pi: data.ch,
+          en: data.en,
+        };
+      });
+      return newState;
+    });
+  };
+
+  // ピンインモード
+  const pin = () => {
+    // 既にピンインモードの場合は処理しない
+    if (!questions[0].en) return;
+    setQuestions(materialQuestion);
+    setQuestions((prevState) => {
+      const newState = prevState.map((data) => {
+        return {
+          ch: data.pi,
+          pi: data.en,
+        };
+      });
+      return newState;
+    });
+  };
+  // reset
+  const reset = () => {
+    setQuestions(materialQuestion);
+  };
+
+  return {
+    handleOnClick,
+    removeActiveClass,
+    questions,
+    shuffleButton,
+    reverseQuestion,
+    pin,
+    reset,
+  };
+};
+
+// シュッフル機能
+function shuffle(a: any) {
+  var j, x, i;
+  for (i = a.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    x = a[i];
+    a[i] = a[j];
+    a[j] = x;
+  }
+  return a;
+}
